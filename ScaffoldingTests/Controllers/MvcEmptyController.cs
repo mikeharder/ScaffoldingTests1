9c10ead3eaac2e1fa520a7d1ba96104d746f89c1
Mike Harder <mharder@microsoft.com>
2017-07-25 18:31:39 -0700

    Move Empty controller to "Controllers" namespace

--- ScaffoldingTests/Controllers/MvcEmptyController.cs
+++ ScaffoldingTests/Controllers/MvcEmptyController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ScaffoldingTests
+namespace ScaffoldingTests.Controllers
 {
     public class MvcEmptyController : Controller
     {
